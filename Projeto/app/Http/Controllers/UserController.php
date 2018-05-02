ebdde53f197871a75c3390e395c3492a5cde6ed7
Amanda Vieira <2160832@my.ipleiria.pt>
2018-05-02 11:14:01 +0100

    UserController e routes iniciais

---
+++ Projeto/app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function listAllUsersToAdmin() {
+    	
+    }
+
+    public function blockUser($user) {
+
+    }
+
+    public function unblockUser($user) {
+
+    }
+
+    public function promoteUser($user) {
+
+    }
+
+    public function demoteUser($user) {
+    	
+    }
+}
--- Projeto/routes/web.php
+++ Projeto/routes/web.php
@@ -14,3 +14,29 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+ // mostra lista de todos os users (mesmo users bloqueados e administradores) mostra nome, email, type e status (bloqueado ou não)
+Route::get('/users', 'UserController@listAllUsersToAdmin')->name('list.of.all.users');
+
+Route::patch('/users/{user}/block', 'UserController@blockUser')->name('block.user');
+
+Route::patch('/users/{user}/unblock', 'UserController@unblockUser')->name('unblock.user');
+
+ // promove user para admin
+Route::patch('/users/{user}/promote', 'UserController@promoteUser')->name('promote.user');
+
+  // tira admin do user
+Route::patch('/users/{user}/demote', 'UserController@demoteUser')->name('demote.user');
+
+Route::patch('/me/password', 'UserController@changePassword')->name('me.password');
+
+ // mudar perfil 
+Route::patch('/me/profile', 'UserController@meProfile')->name('me.profile');
+
+
+
