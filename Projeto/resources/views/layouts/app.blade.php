0e8102642d736f7739b6c59c0fc3ad15a5cb3ec0
IolandaRosa <2161477@my.ipleiria.pt>
2018-05-06 17:06:06 +0100

    Layout mais homogeneo

--- Projeto/resources/views/layouts/app.blade.php
+++ Projeto/resources/views/layouts/app.blade.php
@@ -24,12 +24,6 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                @auth
-                <h1>Meu Perfil</h1>
-                @endauth
-                <!--<a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Accounts') }}
-                </a>-->
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
