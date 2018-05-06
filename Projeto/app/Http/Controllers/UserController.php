2c424f646057e266a59d33c386cfc02d7358bdb4
IolandaRosa <2161477@my.ipleiria.pt>
2018-05-06 12:25:17 +0100

    Fiz asneira

--- Projeto/app/Http/Controllers/UserController.php
+++ Projeto/app/Http/Controllers/UserController.php
@@ -83,7 +83,123 @@ class UserController extends Controller
         //
     }
 
+    public function listAllUsersToAdmin(Request $request) {
 
+        $pagetitle = "List of Users";
+
+        $users=UserController::filter($request);
+
+        return view('users.listUsersToAdmin', compact('users', 'pagetitle')); 
+        
+    }
+
+    private static function filter(Request $request){
+        //se nada estiver prenchido
+        if(!$request->filled('name') && !$request->filled('type') && !$request->filled('status')){
+           return User::all(); 
+        }
+
+        //se type ou status invalido
+        if(($request->filled('type') && $request->query('type')!='admin' && $request->query('type')!='normal') || ($request->filled('status') && $request->query('status')!='blocked' && $request->query('status')!='unblocked')){
+
+            return User::all();
+        }
+
+        //só type normal
+        if(!$request->filled('name')&& $request->filled('type') && $request->query('type')=='normal' && !$request->filled('status')){
+            return User::where('admin','=',false)->get();
+        }
+        //só type admin
+        if(!$request->filled('name')&& $request->filled('type') && $request->query('type')=='admin' && !$request->filled('status')){
+
+            return User::where('admin','=',true)->get();
+        }
+
+        //só status blocked
+        if(!$request->filled('name')&& !$request->filled('type') && $request->filled('status') && $request->query('status')=='blocked'){
+            return User::where('blocked','=',true)->get();
+        }
+
+        //só status unblocked
+        if(!$request->filled('name')&& !$request->filled('type') && $request->filled('status') && $request->query('status')=='unblocked'){
+            return User::where('blocked','=',false)->get();
+        }
+
+        //type+status
+        if(!$request->filled('name')&& $request->filled('type') && $request->query('type')=='normal' && $request->filled('status')){
+            //'normal'+'blocked'
+            if($request->query('status')=='blocked'){
+                return User::where('admin','=',false)->where('blocked','=',true)->get();
+            }
+            //'normal'+'unblocked'
+            else{
+                return User::where('admin','=',false)->where('blocked','=',false)->get();
+            }
+        }
+
+        if(!$request->filled('name')&& $request->filled('type') && $request->query('type')=='admin' && $request->filled('status')){
+            //'admin'+'blocked'
+            if($request->query('status')=='blocked'){
+                return User::where('admin','=',true)->where('blocked','=',true)->get();
+            }
+            //'admin'+'unblocked'
+            else{
+                return User::where('admin','=',true)->where('blocked','=',false)->get();
+            }
+        }
+
+        //só nome
+        if($request->filled('name') && !$request->filled('type') && !$request->filled('status')){
+            return User::where('name','like','%'.$request->query('name').'%')->get();
+        }
+
+        //nome+type
+        if($request->filled('name') && $request->filled('type') && !$request->filled('status')){
+            //'normal'
+            if($request->query('type')=='normal'){
+                return User::where('name','like','%'.$request->query('name').'%')->where('admin','=',false)->get();
+            }
+            else{
+                //'admin'
+                return User::where('name','like','%'.$request->query('name').'%')->where('admin','=',true)->get();
+            }
+        }
+        //nome+status
+        if($request->filled('name') && !$request->filled('type') && $request->filled('status')){
+            //blocked
+            if($request->query('status')=='blocked'){
+                return User::where('name','like','%'.$request->query('name').'%')->where('blocked','=',true)->get();
+            }
+            else{
+                //unblocked
+                return User::where('name','like','%'.$request->query('name').'%')->where('blocked','=',false)->get();
+            }
+        }
+         //nome+type+status
+        if($request->filled('name') && $request->filled('type') && $request->filled('status')){
+            //'nome'+'normal'+'blocked'
+            if($request->query('type')=='normal'){
+                if($request->query('status')=='blocked'){
+                    return User::where('name','like','%'.$request->query('name').'%')->where('admin','=',false)->where('blocked','=',true)->get();
+                }
+                 //'nome'+'normal'+'unblocked'
+                else{
+                    return User::where('name','like','%'.$request->query('name').'%')->where('admin','=',false)->where('blocked','=',false)->get();
+                }
+            }
+            //'nome'+'admin'+'blocked'
+            else{
+                if($request->query('status')=='blocked'){
+                    var_dump(5.111);
+                    return User::where('name','like','%'.$request->query('name').'%')->where('admin','=',true)->where('blocked','=',true)->get();
+                }
+            //'nome'+'admin'+'unblocked'
+                else{
+                    return User::where('name','like','%'.$request->query('name').'%')->where('admin','=',true)->where('blocked','=',false)->get();
+                }
+            }
+        }
+    }
 
 
 
